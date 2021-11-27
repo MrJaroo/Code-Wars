c8fcfdcd180ff25b93d4642ba6469b0e53bf4989
MrJaroo <mishadolininma@mail.ru>
2021-11-27 16:26:46 +0400

    what the real floor codewars

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+function one(n) {
+    if (n <= 0) return n;
+    else if (n >= 13) {
+        return n - 2;
+    }
+    else {
+        return n - 1;
+    }
+}
+
+console.log(one(15))
