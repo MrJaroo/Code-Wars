91a7175dd8306dca32ba5228bcc35af1d436909d
MrJaroo <mishadolininma@mail.ru>
2021-11-29 14:47:59 +0400

    add codewars years

--- main.js
+++ main.js
@@ -8,4 +8,36 @@ function one(n) {
     }
 }
 
-console.log(one(15))
+console.log(one(15));
+
+function rentalCarCost(d) {
+    if (d >= 3 && d < 6) {
+        return (40 * d) - 20;
+    }
+    else if (d >= 7) {
+        return (40 * d) - 50
+    }
+
+    else {
+        return 40 * d;
+    }
+
+}
+
+console.log(rentalCarCost(3));
+console.log('Next')
+
+century = year => {
+    if (year.lenght < 3) {
+        return 1;
+    }
+    else if (year % 100 === 0) {
+        return year / 100
+    }
+
+    return Math.floor(year / 100) + 1
+
+}
+
+console.log(century(1601));
+
